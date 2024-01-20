cb01ead6ec39dd6f1e491311e77246573b79e63f
Srasulova <rasulova_sabina@yahoo.com>
2024-01-19 20:40:47 -0500

    part 3 and 4

--- index.js
+++ index.js
@@ -20,3 +20,32 @@ class Car extends Vehicle {
         this.numWheels = 4
     }
 }
+
+class Motorcycle extends Car {
+constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 2
+}
+
+revEngine() {
+    return "VROOM!!!"
+}
+}
+
+class Garage{
+    constructor(capacity) {
+        this.vehicles =[];
+        this.capacity = capacity
+    }
+
+    add(newVehicle) {
+        if(!newVehicle instanceof Vehicle) {
+            return "Only vehicles are allowed!"
+        }
+        if(this.vehicles.length >= this.capacity) {
+            return "Sorry, we're full!"
+        }
+        this.vehicles.push(newVehicle);
+        return "Vehicle added!"
+    }
+}
